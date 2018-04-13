90549d1a289c20e89a068bab6b2cfa9add2c76f5
wellzhen <moonquake@163.com>
2018-04-13 15:37:05 +0800

    avl: binary_tree

---
+++ cpp/avl_binary_tree.cpp
@@ -0,0 +1,77 @@
+#include "stdafx.h"
+
+
+class Avl
+{
+public:
+	typedef struct _NODE {
+		int nData;
+		_NODE* pLchild;
+		_NODE* pRchild;
+	} NODE;
+	bool insert(int nData);
+	
+	void preOrder(); 
+	void midOrder();
+	void postOrder(); 
+
+private:
+	bool insertNode(NODE* & pNode, int nData);
+	int getHeight(NODE* & pNode);
+	void preOrderTraverse(NODE*& pNode);
+	void midOrderTraverse(NODE*& pNode);
+	void postOrderTraverse(NODE*& pNode);
+private:
+	NODE* m_pRoot;
+	int m_nCount;
+};
+
+bool Avl::insert(int nData)
+{
+	bool res = insertNode(m_pRoot, nData);
+	return res;
+}
+
+bool Avl::insertNode(NODE* & pNode, int nData)
+{
+	if (pNode == NULL) {
+		pNode = new NODE{};
+		pNode->nData = nData;
+		m_nCount++;
+		return true;
+	}
+
+	if (getHeight(pNode->pLchild) >= getHeight(pNode->pRchild)) {//insert into left
+		insertNode(pNode->pLchild, nData);
+	}
+	else {
+		insertNode(pNode->pRchild, nData);
+	}
+	return false;
+}
+
+int Avl::getHeight(NODE* & pNode)
+{
+	if (pNode == NULL) {
+		return 0;
+	}
+	int nLeftHeight = getHeight(pNode->pLchild);
+	int nRightHeight = getHeight(pNode->pRchild);
+
+	return (nLeftHeight > nRightHeight ? nLeftHeight : nRightHeight) + 1;
+}
+
+void Avl::preOrder()
+{
+	preOrderTraverse(m_pRoot);
+}
+
+void Avl::preOrderTraverse(NODE*& pNode)
+{
+	if (pNode == NULL) {
+		return;
+	}
+	//root
+
+
+}
